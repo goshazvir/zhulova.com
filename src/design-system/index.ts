2e04cbaf3bf7428d78fe02e48f27f177001daf45
Heorhii <goshazvir@gmail.com>
2026-06-18 21:15:39 +0200

    feat: add design-system barrel export and README

---
+++ src/design-system/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Modal } from './Modal';
+export { default as Card } from './Card';
+export { Heading, Text } from './Typography';
+export { Section, Container } from './Section';
+export { Badge, Stat } from './Badge';
